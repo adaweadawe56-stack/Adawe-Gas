5d3bab453a8bc90bfbb360387d36c47675d085b4
adaweadawe56-stack <adaweadawe56@gmail.com>
2026-07-04 11:05:21 +0300

    Update orderTracking.js

--- orderTracking.js
+++ orderTracking.js
@@ -418,7 +418,7 @@ if (sellerSnap.exists()) {
 
   await updateDoc(sellerRef, {
     totalRatings,
-    averageRating: Number(averageRating.toFixed(1))
+    averageRating: Number(averageRating.toFixed(2))
   });
 
   console.log("Seller updated successfully");
