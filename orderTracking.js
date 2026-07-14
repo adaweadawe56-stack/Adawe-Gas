f27995f5a2770bf2661be6cf05477063a5708fdd
adaweadawe56-stack <adaweadawe56@gmail.com>
2026-07-14 10:47:47 +0300

    Update orderTracking.js

--- orderTracking.js
+++ orderTracking.js
@@ -133,7 +133,7 @@ console.log("Previous:", previousStatus);
 console.log("Current:", data.status);
 
 if (
-    previousStatus !== undefined &&
+    previousStatus !== null &&
     previousStatus !== data.status &&
     Notification.permission === "granted"
 ) {
